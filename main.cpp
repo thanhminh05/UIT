7eeafdb85707dff0e45281a9333a761d8959b0a9
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2024-06-13 21:29:38 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int a;
+    cin >> a; 
+
+
+    cout << "Hello : " << a;
+    return 0;
+}
