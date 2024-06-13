d20e7906b2c7bb9b96efa6e39c5ab426ceec7772
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2024-06-13 22:07:53 +0700

    config

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    
-    int a;
-    cin >> a; 
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
 
+  int a;
+  cin >> a;
 
-    cout << "Hello : " << a;
-    return 0;
+  cout << "Hello : " << a;
+  return 0;
 }
